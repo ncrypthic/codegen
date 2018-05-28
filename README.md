8e7f21316b8de8e212bde61d1e7954d7fa0460f5
Lim Afriyadi <lim.afriyadi.id@gmail.com>
2018-05-28 15:33:28 +0700

    Add example and description

--- README.md
+++ README.md
@@ -3,6 +3,13 @@ Codegen
 
 Codegen is collection of code generation tools for golang
 
+List
+----
+
+- contextgen
+  `contextgen` is a command line to modify existing code to import `context.Context`
+  package and append it as first argument to every functions in the file.
+
 License
 -------
 MIT
---
+++ contextgen/example/test.go
@@ -0,0 +1,71 @@
+package example
+
+import "fmt"
+
+type TestFn func()
+
+type TestFn2 func(string)
+
+type TestFn3 func(string) error
+
+type TestFn func(string, int64) error
+
+type TestFn func(string, int64) ([]byte, string)
+
+func Test1(id string) {}
+
+func Test2(id, name string) {}
+
+func Test3(id, name string, count int64) {}
+
+func Test4(id string) error { return nil }
+
+func Test5(id, name string) error { return nil }
+
+func Test6(id, name string, count int64) error { return nil }
+
+func Test7(id string) ([]byte, error) { return nil, nil }
+
+func Test8(id, name string) ([]byte, error) { return nil, nil }
+
+func Test9(id, name string, count int64) ([]byte, error) { return nil, nil }
+
+type Interface interface {
+	Test1(id string)
+
+	Test2(id, name string)
+
+	Test3(id, name string, count int64)
+
+	Test4(id string) error
+
+	Test5(id, name string) error
+
+	Test6(id, name string, count int64) error
+
+	Test7(id string) ([]byte, error)
+
+	Test8(id, name string) ([]byte, error)
+
+	Test9(id, name string, count int64) ([]byte, error)
+}
+
+type Struct struct{}
+
+func (s *Struct) Test1(id string) {}
+
+func (s *Struct) Test2(id, name string) {}
+
+func (s *Struct) Test3(id, name string, count int64) {}
+
+func (s *Struct) Test4(id string) error { return nil }
+
+func (s *Struct) Test5(id, name string) error { return nil }
+
+func (s *Struct) Test6(id, name string, count int64) error { return nil }
+
+func (s *Struct) Test7(id string) ([]byte, error) { return nil, nil }
+
+func (s *Struct) Test8(id, name string) ([]byte, error) { return nil, nil }
+
+func (s *Struct) Test9(id, name string, count int64) ([]byte, error) { return nil, nil }
